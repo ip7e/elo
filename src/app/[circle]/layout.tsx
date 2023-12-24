db1cda244537cd673bf156a53a9149dda18738de
Ika Pkhakadze <i.pkhakadze@gmail.com>
2023-12-24 16:29:27 +0100

    Refactor layout component

--- src/app/[circle]/layout.tsx
+++ src/app/[circle]/layout.tsx
@@ -1,22 +1,4 @@
-import type { Metadata } from "next"
-import { Inter, Roboto_Mono } from "next/font/google"
-import "../globals.css"
 import Navigation from "./navigation"
-
-const robotoMono = Roboto_Mono({
-  subsets: ["latin"],
-  variable: "--font-roboto-mono",
-})
-const inter = Inter({
-  weight: "variable",
-  subsets: ["latin"],
-  variable: "--font-inter",
-})
-
-export const metadata: Metadata = {
-  title: "Elo",
-  description: "Board Game elo system",
-}
 
 export default function RootLayout({
   children,
@@ -26,13 +8,9 @@ export default function RootLayout({
   params: { circle: string }
 }) {
   return (
-    <html lang="en">
-      <body className={`${robotoMono.variable} ${inter.variable} font-sans bg-bg dark:bg-black`}>
-        <div className="container max-w-lg py-10 mx-auto">
-          <Navigation slug={params.circle} />
-          {children}
-        </div>
-      </body>
-    </html>
+    <div className="container max-w-lg py-10 mx-auto">
+      <Navigation slug={params.circle} />
+      {children}
+    </div>
   )
 }
---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next"
+import { Inter, Roboto_Mono } from "next/font/google"
+import "./globals.css"
+
+const robotoMono = Roboto_Mono({
+  subsets: ["latin"],
+  variable: "--font-roboto-mono",
+})
+const inter = Inter({
+  weight: "variable",
+  subsets: ["latin"],
+  variable: "--font-inter",
+})
+
+export const metadata: Metadata = {
+  title: "Elo",
+  description: "Board Game elo system",
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={`${robotoMono.variable} ${inter.variable} font-sans bg-bg dark:bg-black`}>
+        {children}
+      </body>
+    </html>
+  )
+}
