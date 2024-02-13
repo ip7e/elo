4b7ae34e0d031f38a32d611fe1bd7f09a1b4f3be
Ika Pkhakadze <i.pkhakadze@gmail.com>
2024-02-13 22:16:29 +0100

    Commented out code for adding a member to a circle

--- src/app/[circle]/control/add-member.action.ts
+++ src/app/[circle]/control/add-member.action.ts
@@ -9,16 +9,16 @@ export async function addMember({ name, circleId }: Props) {
   // TODO:
 
   return { error: "yoo" }
-  const { data, error } = await supabase
-    .from("circle_members")
-    .insert({ circle_id: circleId, name: name })
-    .select("*")
-    .single()
+  // const { data, error } = await supabase
+  //   .from("circle_members")
+  //   .insert({ circle_id: circleId, name: name })
+  //   .select("*")
+  //   .single()
 
-  if (!error) {
-    revalidatePath("/[circle]", "layout")
-    return { data, success: true }
-  }
+  // if (!error) {
+  //   revalidatePath("/[circle]", "layout")
+  //   return { data, success: true }
+  // }
 
-  return { error }
+  // return { error }
 }
