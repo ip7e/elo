13236c45c7cb372bf33e546fd6a2dfd1aabfe726
Ika Pkhakadze <i.pkhakadze@gmail.com>
2024-04-17 14:45:11 +0200

    new navigation

--- src/app/[circle]/navigation.tsx
+++ src/app/[circle]/navigation.tsx
@@ -3,69 +3,50 @@
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 import { Circle } from "./types"
+import Logo from "@/components/logo"
 
 type Props = { circle: Circle; isAdmin: boolean }
 export default function Navigation({ isAdmin, circle }: Props) {
   const currentPathname = usePathname()
-  const slug = circle.slug
+  const circleRoot = `/${circle.slug}`
 
   const routes = [
     {
-      pathname: `/${slug}`,
-      label: "leaderboard",
+      pathname: circleRoot,
+      label: "ranking",
     },
     {
-      pathname: `/${slug}/history`,
+      pathname: `${circleRoot}/history`,
       label: "history",
     },
-    isAdmin && {
-      pathname: `/${slug}/control`,
-      label: "settings",
-    },
-  ].filter(Boolean)
+  ]
+
+  if (isAdmin) {
+    routes.push({
+      pathname: `${circleRoot}/control`,
+      label: "admin",
+    })
+  }
 
   return (
     <div
-      className="flex gap-4 items-center py-8 
-      text-black dark:text-white font-light"
+      className="flex gap-4 py-8 w-full p-4
+      text-black dark:text-white font-light justify-between"
     >
-      <nav className="flex gap-2 relative">
-        {routes.map((route) => (
+      <Link className="flex gap-2 items-center" href={circleRoot}>
+        <Logo className="w-8 text-accent" />
+        <div className="w-0.5 h-4 bg-gray-500/20" />
+        <h1 className="font-semibold text-gray-400">catan</h1>
+      </Link>
+
+      <nav className="flex gap-2 relative font-semibold text-gray-400">
+        {routes.map(({ pathname, label }) => (
           <Link
-            key={route.pathname}
-            href={route.pathname}
-            className={`${
-              currentPathname === route.pathname
-                ? "text-black dark:text-white"
-                : "text-neutral-400 dark:text-neutral-500"
-            }`}
+            href={pathname}
+            key={pathname}
+            className={currentPathname === pathname ? "text-accent" : ""}
           >
-            <div
-              className={`w-8 h-2 rounded-full group 
-            ${
-              currentPathname === route.pathname
-                ? "bg-accent"
-                : `bg-neutral-200 dark:bg-neutral-700
-                hover:bg-neutral-400 
-                dark:hover:bg-neutral-400
-                `
-            }`}
-              key={route.pathname}
-            >
-              <span className="block relative w-full h-full ">
-                <span className="absolute inset-[-8px] rounded-full"></span>
-              </span>
-              <span
-                className={`absolute top-4 left-0 right-0 text-center  
-                opacity-0  pointer-events-none text-sm
-                transition-all duration-150 ease-in-out
-                group-hover:opacity-100
-                ${currentPathname === route.pathname && "text-accent"}
-                `}
-              >
-                {route.label}
-              </span>
-            </div>
+            {label}
           </Link>
         ))}
       </nav>
