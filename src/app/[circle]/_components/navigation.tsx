5bc2c31f838993d05bdf7477892db4771e52a857
Ika Pkhakadze <i.pkhakadze@gmail.com>
2024-08-01 23:35:41 +0200

    remove region info

--- src/app/[circle]/_components/navigation.tsx
+++ src/app/[circle]/_components/navigation.tsx
@@ -8,8 +8,6 @@ import { Circle } from "../../../server/types"
 import { cn } from "@/utils/tailwind/cn"
 
 type Props = { circle: Circle }
-const region = process.env.AWS_REGION
-
 export default function Navigation({ circle }: Props) {
   const currentPathname = usePathname()
   const circleRoot = `/${circle.slug}`
@@ -49,7 +47,6 @@ export default function Navigation({ circle }: Props) {
         </nav>
       </div>
       <div className="flex items-center gap-2">
-        <div className="text-sm font-light">{region || "unknown region"}</div>
         <HasAccess noAuthCallback={<Link href="/auth">login</Link>}>logged in</HasAccess>
       </div>
     </div>
