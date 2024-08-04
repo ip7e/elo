c190d2c01e04c461019a1871bb923eb357743782
Ika Pkhakadze <i.pkhakadze@gmail.com>
2024-08-04 23:15:07 +0200

    add invite member functionality

--- src/app/[circle]/_dashboard/members.tsx
+++ src/app/[circle]/_dashboard/members.tsx
@@ -1,31 +1,24 @@
 "use client"
 
-import { kickMember } from "@/server/actions"
-import { cn } from "@/utils/tailwind/cn"
-import { EllipsisVertical, Info, Trash2 } from "lucide-react"
-import { useState } from "react"
-import { Member, MemberStats } from "../../../server/types"
-import HasAccess from "../_components/has-access"
-import Star from "../_components/star"
-import { MiddleCell, LeadingCell, TrailingCell, Table, TableRow } from "./_components/table"
-import AddNewMember from "./add-new-member"
-import { useServerAction } from "zsa-react"
+import { Dialog, DialogTrigger } from "@/components/ui/dialog"
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog"
-import { Button } from "@headlessui/react"
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip"
+import { kickMember } from "@/server/actions"
+import { cn } from "@/utils/tailwind/cn"
+import { EllipsisVertical, ShieldCheck, Trash2 } from "lucide-react"
+import { useState } from "react"
+import { useServerAction } from "zsa-react"
+import { Member, MemberStats } from "../../../server/types"
+import HasAccess from "../_components/has-access"
+import Star from "../_components/star"
+import InviteDialogContent from "./_components/invite-dialog-content"
+import { LeadingCell, MiddleCell, Table, TableRow, TrailingCell } from "./_components/table"
+import AddNewMember from "./add-new-member"
 
 type Props = {
   circleId: number
@@ -54,6 +47,8 @@ export default function Members({
     {} as Record<number, number>,
   )
 
+  const ownerMembers = members.filter((m) => !!m.user_id).map((m) => m.id)
+
   return (
     <Table>
       {stats.map(({ elo, name, member_id }, i) => (
@@ -75,45 +70,40 @@ export default function Members({
           <TrailingCell>{elo}</TrailingCell>
 
           <HasAccess>
-            <Dialog>
-              <DropdownMenu>
-                <DropdownMenuTrigger
+            {ownerMembers.includes(member_id!) ? (
+              <Tooltip>
+                <TooltipTrigger
                   className={cn(
-                    "absolute -right-5 text-neutral-300 outline-none",
-                    "opacity-0 group-hover:opacity-100 data-[state=open]:opacity-100",
-                    "hover:text-neutral-600 data-[state=open]:text-neutral-600",
+                    "absolute -right-6 pl-2 text-neutral-300 outline-none",
+                    "opacity-0 transition-opacity group-hover:opacity-100",
+                    "dark:text-neutral-600",
                   )}
                 >
-                  <EllipsisVertical size={16} />
-                  {/* <ShieldCheck size={16} /> */}
-                </DropdownMenuTrigger>
-                <DropdownMenuContent side="bottom" align="end" className="font-sans">
-                  <DialogTrigger>
-                    <DropdownMenuItem>Invite as owner</DropdownMenuItem>
-                  </DialogTrigger>
-                </DropdownMenuContent>
-              </DropdownMenu>
-              <DialogContent className="font-sans">
-                <DialogHeader>
-                  <DialogTitle>Are you absolutely sure?</DialogTitle>
-                </DialogHeader>
-
-                <DialogDescription>
-                  <form onSubmit={(e) => e.preventDefault()}>
-                    <label className="block">
-                      <input
-                        className="bg-background mt-1 block w-full rounded-md border-neutral-300 px-3 py-2 text-base outline-none placeholder:text-neutral-300 focus:border-accent focus:ring-accent dark:border-neutral-600 dark:bg-black dark:placeholder:text-neutral-700"
-                        placeholder="Enter email"
-                      />
-                    </label>
-                  </form>
-                </DialogDescription>
-
-                <DialogFooter>
-                  <Button type="submit">Invite</Button>
-                </DialogFooter>
-              </DialogContent>
-            </Dialog>
+                  <ShieldCheck size={16} />
+                </TooltipTrigger>
+                <TooltipContent>Owner</TooltipContent>
+              </Tooltip>
+            ) : (
+              <Dialog>
+                <DropdownMenu>
+                  <DropdownMenuTrigger
+                    className={cn(
+                      "absolute -right-6 pl-2 text-neutral-300 outline-none",
+                      "opacity-0 group-hover:opacity-100 data-[state=open]:opacity-100",
+                      "hover:text-neutral-600 data-[state=open]:text-neutral-600 dark:text-neutral-600 dark:hover:text-neutral-300 dark:data-[state=open]:text-neutral-300",
+                    )}
+                  >
+                    <EllipsisVertical size={16} />
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent side="bottom" align="end" className="font-sans">
+                    <DialogTrigger>
+                      <DropdownMenuItem>Invite as owner</DropdownMenuItem>
+                    </DialogTrigger>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+                <InviteDialogContent circleId={circleId} memberId={member_id!} />
+              </Dialog>
+            )}
           </HasAccess>
         </TableRow>
       ))}
--- src/app/[circle]/layout.tsx
+++ src/app/[circle]/layout.tsx
@@ -2,6 +2,7 @@ import { getCircleBySlug, hasCurrentUserAccessToCircle } from "@/server/queries"
 import { AccessProvider } from "./_context/access-context"
 import Navigation from "./_components/navigation"
 import { Suspense } from "react"
+import { TooltipProvider } from "@/components/ui/tooltip"
 
 export default async function RootLayout({
   children,
@@ -17,23 +18,25 @@ export default async function RootLayout({
 
   return (
     <AccessProvider circle={circle} hasAccess={hasAccess}>
-      <Navigation circle={circle} />
-      <div className="container mx-auto flex h-full max-w-3xl flex-col">
-        <div className="mx-auto mt-5 flex w-full items-center justify-center"></div>
+      <TooltipProvider>
+        <Navigation circle={circle} />
+        <div className="container mx-auto flex h-full max-w-3xl flex-col">
+          <div className="mx-auto mt-5 flex w-full items-center justify-center"></div>
 
-        <div className="flex-1">
-          <Suspense
-            fallback={
-              <div className="flex h-3/4 w-full flex-col justify-center text-center font-mono text-neutral-300">
-                loading...
-              </div>
-            }
-          >
-            {children}
-          </Suspense>
+          <div className="flex-1">
+            <Suspense
+              fallback={
+                <div className="flex h-3/4 w-full flex-col justify-center text-center font-mono text-neutral-300">
+                  loading...
+                </div>
+              }
+            >
+              {children}
+            </Suspense>
+          </div>
+          <div className="max-h-28 flex-1"></div>
         </div>
-        <div className="max-h-28 flex-1"></div>
-      </div>
+      </TooltipProvider>
     </AccessProvider>
   )
 }
