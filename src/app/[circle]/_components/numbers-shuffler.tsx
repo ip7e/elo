44f1c4f59b07ee4cc0339dbbd3ea15c89018dc6e
Ika Pkhakadze <i.pkhakadze@gmail.com>
2024-08-08 21:28:00 +0200

    numbers animation

--- src/app/[circle]/_components/numbers-shuffler.tsx
+++ src/app/[circle]/_components/numbers-shuffler.tsx
@@ -1,30 +1,50 @@
-import React, { useState, useEffect } from "react"
+import { animate, useMotionValue, useMotionValueEvent, useTransform } from "framer-motion"
+import { useEffect, useRef, useState } from "react"
 
-const RandomSymbols = ["^", "@", "%", "#", "&", "^", "!", "@", "#"]
+const generateRandomSymbols = (length: number, existing: string) => {
+  const randomSymbols = (Math.random() * 100000).toString().slice(0, length)
+  const newValue = existing.slice(0, -length) + randomSymbols
+  return newValue
+}
 
 const SymbolDisplay = ({ isLoading, value }: any) => {
-  const [loadingSymbols, setLoadingSymbols] = useState(["", "", "", ""])
+  const [displayValue, setDisplayValue] = useState(value.toString())
+  const [isAnimating, setIsAnimating] = useState(false)
+
+  const nRef = useRef(0)
+
+  const valueRef = useRef<string>(value.toString())
+  valueRef.current = value.toString()
+
+  const v = useMotionValue(0)
+  const vt = useTransform(v, (n) => Math.round(n))
+
+  useMotionValueEvent(vt, "change", (n) => {
+    console.log(n)
+    if (n) nRef.current = n
+    setIsAnimating(Boolean(n))
+  })
 
   useEffect(() => {
-    let intervalId: any
-    if (isLoading) {
-      intervalId = setInterval(() => {
-        setLoadingSymbols(RandomSymbols.sort(() => Math.random() - 0.5).slice(0, 4))
-      }, 250)
+    let interval: any
+
+    if (isAnimating) {
+      interval = setInterval(() => {
+        const newValue = generateRandomSymbols(nRef.current, valueRef.current)
+        setDisplayValue(newValue)
+      }, 50)
     } else {
-      clearInterval(intervalId)
+      clearInterval(interval)
     }
+    return () => clearInterval(interval)
+  }, [isAnimating])
 
-    return () => clearInterval(intervalId)
-  }, [isLoading])
+  useEffect(() => {
+    if (isLoading) animate(v, 4, { duration: 1 })
+    else animate(v, 0, { duration: 1 })
+  }, [isLoading, v])
 
-  return (
-    <>
-      {isLoading
-        ? loadingSymbols.map((symbol, index) => <span key={index}>{symbol}</span>)
-        : value.toString().padStart(4, "0")}
-    </>
-  )
+  return <>{displayValue}</>
 }
 
 export default SymbolDisplay
