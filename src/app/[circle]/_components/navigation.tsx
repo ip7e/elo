77c87a7f9b444b1fd3bb91e5c36a2b4764cf49f2
Ika Pkhakadze <i.pkhakadze@gmail.com>
2024-08-09 17:06:49 +0200

    logo yoo

--- src/app/[circle]/_components/navigation.tsx
+++ src/app/[circle]/_components/navigation.tsx
@@ -5,6 +5,7 @@ import Link from "next/link"
 import { usePathname } from "next/navigation"
 import { Circle } from "../../../server/types"
 import HasAccess from "./has-access"
+import Logo from "@/components/logo"
 
 type Props = { circle: Circle }
 export default function Navigation({ circle }: Props) {
@@ -23,29 +24,29 @@ export default function Navigation({ circle }: Props) {
   ]
 
   return (
-    <div className="fixed top-0 z-auto flex w-full justify-between gap-4 p-4 py-4 font-medium text-gray-400">
-      <div className="flex gap-4">
-        <Link href={circleRoot} className="flex items-center gap-2">
-          {/* <Logo className="w-8 text-accent" />
-           */}
-          <h1>shmelo</h1>
+    <div className="fixed top-0 z-auto flex w-full justify-between gap-4 p-4 py-4 font-medium text-gray-400 dark:text-gray-300">
+      <div className="flex items-start gap-4">
+        <Logo className="text-gray-600 dark:text-gray-200" />
+        <div className="flex items-center gap-4">
+          <Link href={circleRoot} className="flex h-fit items-center gap-2">
+            <h1 className="ml-2">{circle.name.toLowerCase()}</h1>
+          </Link>
           <div className="h-4 w-0.5 bg-gray-400/20 dark:bg-gray-200/30" />
-          <h1>{circle.name.toLowerCase()}</h1>
-        </Link>
 
-        <nav className="flex gap-2">
-          {routes.map(({ pathname, label }) => (
-            <Link
-              href={pathname}
-              key={pathname}
-              className={cn(currentPathname === pathname && "text-accent")}
-            >
-              {label}
-            </Link>
-          ))}
-        </nav>
+          <nav className="flex gap-2">
+            {routes.map(({ pathname, label }) => (
+              <Link
+                href={pathname}
+                key={pathname}
+                className={cn(currentPathname === pathname && "text-accent")}
+              >
+                {label}
+              </Link>
+            ))}
+          </nav>
+        </div>
       </div>
-      <div className="flex items-center gap-2">
+      <div className="flex items-start gap-2">
         <HasAccess noAuthCallback={<Link href="/auth">login</Link>}>logged in</HasAccess>
       </div>
     </div>
