aff1b99476c41fdc8964becb86bd8fd498e55722
Ika Pkhakadze <i.pkhakadze@gmail.com>
2024-10-19 19:57:47 +0200

    cicle admin procedure

--- src/server/procedures.ts
+++ src/server/procedures.ts
@@ -1,5 +1,7 @@
 import { createServerClientWithCookies } from "@/utils/supabase/server"
 import { createServerActionProcedure } from "zsa"
+import createSuperClient from "./supabase"
+import { z } from "zod"
 
 export const authedProcedure = createServerActionProcedure().handler(async () => {
   const supabase = createServerClientWithCookies()
@@ -9,3 +11,21 @@ export const authedProcedure = createServerActionProcedure().handler(async () =>
 
   return { user: data.user }
 })
+
+export const circleAdminProcedure = createServerActionProcedure(authedProcedure)
+  .input(z.object({ circleId: z.number() }))
+  .handler(async ({ input, ctx }) => {
+    const supabase = createSuperClient()
+
+    const { data: memberWithCircle } = await supabase
+      .from("circle_members")
+      .select("*, circle:circles(*)")
+      .eq("circle_id", input.circleId)
+      .eq("user_id", ctx.user.id)
+      .single()
+
+    if (!memberWithCircle) throw "Has no access to this circle"
+
+    const { circle, ...member } = memberWithCircle
+    return { member, circle, user: ctx.user }
+  })
