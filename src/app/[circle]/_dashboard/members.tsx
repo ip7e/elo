51cff538032149d55d4d4636f3ffb8f52bc2b779
Ika Pkhakadze <i.pkhakadze@gmail.com>
2025-05-22 14:09:12 +0400

    fix dynamic date

--- src/app/[circle]/_dashboard/members.tsx
+++ src/app/[circle]/_dashboard/members.tsx
@@ -111,7 +111,7 @@ export default function Members({
           className="group absolute -top-12 left-1/2 flex -translate-x-1/2 cursor-pointer items-center gap-2 text-nowrap rounded-md border border-border bg-background px-3 py-1.5 shadow-sm"
           onClick={() => onGameSelect(null)}
         >
-          <span className="text-sm font-medium text-muted-foreground">December 28</span>
+          <span className="text-sm font-medium text-muted-foreground">{formattedDate}</span>
           <X className="h-3 w-3 text-muted group-hover:text-primary" />
         </div>
       )}
